1c71cdac8f0e74699830c28380c3f3dce8027546
Christopher Siems <csiems@gmail.com>
2018-09-29 11:26:48 -0700

    create playground class with ability to add play sites

---
+++ src/Playground.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Playground {
+    private ArrayList<PlaySite> playSites;
+    private int capacity;
+
+    public Playground() {
+        playSites = new ArrayList<>();
+        capacity = 0;
+    }
+
+    public void addPlaySite(PlaySite playSite) {
+        playSites.add(playSite);
+        capacity += playSite.getCapacity();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+}
