77f483a8a003be7222b6102492093f62522e7a3d
Christopher Siems <csiems@gmail.com>
2018-09-29 11:35:05 -0700

    allow user to determine length of a site visit

--- src/Visit.java
+++ src/Visit.java
@@ -25,4 +25,17 @@ public class Visit {
     public void isExited() {
         timeExited = new Date();
     }
+
+    /**
+     * Finds the length of time spent on a particular visit.
+     * @return long representing length of visit in milliseconds, or
+     *         -1 if the visit has not ended.
+     */
+    public long getVisitLength() {
+        if (timeExited != null) {
+            return timeExited.getTime() - timeEntered.getTime();
+        } else {
+            return -1;
+        }
+    }
 }
