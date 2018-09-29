ab051b729616136e05a74668005022d21a0f3d0b
Christopher Siems <csiems@gmail.com>
2018-09-29 13:29:31 -0700

    create addkid tests in playsite class

--- tests/src/PlaySiteTest.java
+++ tests/src/PlaySiteTest.java
@@ -2,31 +2,48 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class PlaySiteTest {
+class PlaySiteTest {
 
     @Test
     void addKid_CorrectlyAddsKidIfThereIsCapacity_ReturnsZero() {
-
+        DoubleSwingSite swings = new DoubleSwingSite(1);
+        Kid kid = new Kid(true);
+        int result = swings.addKid(kid);
+        assertEquals(0, result);
     }
 
     @Test
     void addKid_CorrectlyHandlesKidsWhoRejectQueueWhenCapacityReached_ReturnsNegativeOne() {
-
+        DoubleSwingSite swings = new DoubleSwingSite(0);
+        Kid kid = new Kid(false);
+        int result = swings.addKid(kid);
+        assertEquals(-1, result);
     }
 
     @Test
     void addKid_WillNotAddKidToTheSiteASecondTime_ReturnsZero() {
-
+        DoubleSwingSite swings = new DoubleSwingSite(1);
+        Kid kid = new Kid(true);
+        int result = swings.addKid(kid);
+        swings.addKid(kid);
+        assertEquals(0, result);
     }
 
     @Test
-    void addKid_WillNotAddKidToTheQueueASecondTime_ReturnsTwo() {
-
+    void addKid_WillNotAddKidToTheQueueASecondTime_ReturnsOne() {
+        DoubleSwingSite swings = new DoubleSwingSite(0);
+        Kid kid = new Kid(true);
+        int result = swings.addKid(kid);
+        swings.addKid(kid);
+        assertEquals(1, result);
     }
 
     @Test
     void addKid_CorrectlyAddsKidToQueueWhenCapacityReached_ReturnsOne() {
-
+        DoubleSwingSite swings = new DoubleSwingSite(0);
+        Kid kid = new Kid(true);
+        int result = swings.addKid(kid);
+        assertEquals(1, result);
     }
 
 
