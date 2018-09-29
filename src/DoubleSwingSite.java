7b1b8e37c7fa1723cb8880dcf92a84329d24470b
Christopher Siems <csiems@gmail.com>
2018-09-29 11:35:42 -0700

    create a double swing site that can be added to playground

---
+++ src/DoubleSwingSite.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class DoubleSwingSite extends PlaySite {
+
+    private int numberOfSwings;
+
+    public DoubleSwingSite(int numberOfSwings) {
+        this.numberOfSwings = numberOfSwings;
+        // Each double swing in the site can hold two children
+        this.capacity = this.numberOfSwings * 2;
+    }
+
+    public int getNumberOfSwings() {
+        return numberOfSwings;
+    }
+
+    public void addSwing() {
+        numberOfSwings += 1;
+    }
+
+    public void removeSwing() {
+        if (numberOfSwings > 1) {
+            numberOfSwings -= 1;
+        } else {
+            numberOfSwings = 0;
+        }
+    }
+}
