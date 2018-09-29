b16fbfee7568ea0d53ae0291127c5679e331131f
Christopher Siems <csiems@gmail.com>
2018-09-29 11:46:24 -0700

    improve todo on carousel site class

--- src/CarouselSite.java
+++ src/CarouselSite.java
@@ -1,12 +1,10 @@
 public class CarouselSite extends PlaySite {
 
-    private int capacity;
-
-    public CarouselSite(int capacity) {
-        this.capacity = capacity;
+    public CarouselSite(int numberOfAnimals) {
+        // Assumes carousel animals can hold only one kid
+        // TODO: May want to add carousel animals, each with
+        // TODO: their own capacity that would then be summed
+        // TODO: for carousel capacity.
+        this.capacity = numberOfAnimals;
     }
-
-    /*TODO: May want to add carousel animals, each with their own
-      capacity that would then be summed for carousel capacity.   */
-
 }
