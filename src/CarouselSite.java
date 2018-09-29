1597d56c6436c2706a613f0645df5e6cd3864c0f
Christopher Siems <csiems@gmail.com>
2018-09-29 11:36:07 -0700

    add carousel site that can be added to a playground

---
+++ src/CarouselSite.java
@@ -0,0 +1,12 @@
+public class CarouselSite extends PlaySite {
+
+    private int capacity;
+
+    public CarouselSite(int capacity) {
+        this.capacity = capacity;
+    }
+
+    /*TODO: May want to add carousel animals, each with their own
+      capacity that would then be summed for carousel capacity.   */
+
+}
