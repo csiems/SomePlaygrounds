baf73868487c2bf33925007d11550f0111765113
Christopher Siems <csiems@gmail.com>
2018-09-29 11:48:22 -0700

    add ball pit site that can be added to playground

---
+++ src/BallPitSite.java
@@ -0,0 +1,18 @@
+public class BallPitSite extends PlaySite {
+
+    public BallPitSite(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void addCapacity(int additionalCapacity) {
+        capacity += additionalCapacity;
+    }
+
+    public void removeCapacity(int removedCapacity) {
+        if (capacity >= removedCapacity) {
+            capacity -= removedCapacity;
+        } else {
+            capacity = 0;
+        }
+    }
+}
